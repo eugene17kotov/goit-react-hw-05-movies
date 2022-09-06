ec76085a483a3aecca796d67bfff5ce98bbac385
Yevhenii Kobets <eu.kobets@gmail.com>
2022-09-06 09:17:31 +0300

    create start template

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+// import { useEffect, useState } from 'react';
+// import { Box } from 'utils/Box';
+
+// import { ... } from 'services/api';
+
+export const App = () => {
+  // const [images, setImages] = useState([]);
+
+  return;
+};
---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+// export const getFetchImages = async (query, page = 1) => {
+//   const response = await axios({
+//     params: {
+//       key: '28299766-d83cf6cfdb58eecdb0caa94e4',
+//       image_type: 'photo',
+//       orientation: 'horizontal',
+//       page,
+//       per_page: 12,
+//       q: query,
+//     },
+//   });
+
+//   return response.data;
+// };
